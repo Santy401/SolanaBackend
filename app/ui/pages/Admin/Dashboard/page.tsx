77cc7dfd4e624bbe05d41a9172bc38f87f44e89d
Santy401 <santiprocastellar6@gmail.com>
2025-12-01 23:36:13 -0500

    Update Dashboard page with greeting and options
    - Refactor Dashboard page to client component
    - Add personalized greeting message
    - Integrate OptionsTargets component for navigation
    - Improve dashboard layout and styling

--- app/ui/pages/Admin/Dashboard/page.tsx
+++ app/ui/pages/Admin/Dashboard/page.tsx
@@ -1,9 +1,17 @@
+'use client';
+
+import { OptionsTargets } from "@/app/ui/components/Dashboard/OptionsTargets"
+
 export default function Dashboard() {
-  return (
-    <div className="flex bg-[#5486ED] rounded-2xl min-h-screen">
-      <div className="bg-white flex-1 h-[90vh] relative top-2 rounded-2xl">
-        {/* Tu contenido aquí */}
-      </div>
-    </div>
-  )
+    return (
+        <div className="p-4">
+            <h1 className="text-4xl text-white font-bold mb-6">Dashboard</h1>
+            <div className="p-3 bg-[#1c1c1c] rounded my-5">
+                <span className="text-white">Buenas Noches <span className="font-bold">Santiago Castellar</span></span>
+            </div>
+            <div className="overflow-auto active:cursor-grabbing mx-auto my-auto">
+                <OptionsTargets />
+            </div>
+        </div>
+    )
 }
